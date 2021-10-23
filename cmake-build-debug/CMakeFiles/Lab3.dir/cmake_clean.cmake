e4483c15b5f1894e2f45e49c42b3438248138352
mehak <aroramehak2311@gmail.com>
2021-10-23 12:06:19 -0400

    First Commit

---
+++ cmake-build-debug/CMakeFiles/Lab3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab3.dir/main.cpp.obj"
+  "Lab3.exe"
+  "Lab3.exe.manifest"
+  "Lab3.pdb"
+  "libLab3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
