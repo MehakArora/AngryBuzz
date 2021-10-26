c4816869ef83ca2b9dad1f5124daf541f5dc839c
mehak <aroramehak2311@gmail.com>
2021-10-26 14:09:40 -0400

    Score Flying

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
-//
-// Created by mehak on 10/23/2021.
-//
+/*
+Author: Mehak Arora
+Class: ECE 6122
+Last Date Modified: 10/26/2021
+
+Description:
+Code for the game 'Buzzy's Revenge'
+
+*/
 
 #include <sstream>
 #include <iostream>
@@ -17,13 +23,56 @@ using namespace sf;
 #define GEORGIABULLDOG 9;
 #define TIGER 0;
 #define PI 3.14159265
+const int totalLives = 5;
+int column1[5], column2[5];
+Sprite spriteCreature[10];
+Text plusScore;
+Vector2f velocity;
 
+
+void Shuffle(int seed, Vector2f creaturePos[10]){
+    /*
+     * Randomly Shuffle the creatures on screen
+     */
+    int index;
+    int creatureRest[8] = {1,2,3,4,5,6,7,8};
+    srand(time(0) + seed);
+    if(rand()%2 == 0)
+    {
+        column1[4] = TIGER;
+        column2[4] = GEORGIABULLDOG;
+    }
+    else
+    {
+        column1[4] = GEORGIABULLDOG;
+        column2[4] = TIGER;
+    }
+
+    shuffle(creatureRest, creatureRest + 8, std::default_random_engine(time(0) + 2 + seed));
+    for (int i =0; i < 4; i++)
+    {
+        column1[i] = creatureRest[i];
+        column2[i] = creatureRest[i+4];
+    }
+    shuffle(column1, column1 + 5, std::default_random_engine(time(0) + seed));
+    shuffle(column2, column2 + 5, std::default_random_engine(time(0) + 1 + seed));
+
+    for (int i = 0; i < 10 ; i++)
+    {
+        index = (i < 5) ? column1[i]:column2[i-5];
+        spriteCreature[index].setPosition(creaturePos[i].x, creaturePos[i].y);
+        spriteCreature[index].setRotation(0);
+    }
+}
 
 int main()
 {
+
     // Create a video mode object
     VideoMode vm(1920, 1080);
+    //const VideoMode gameDisplay = VideoMode::getDesktopMode();
 
+    //std::cout<<gameDisplay.width << " " << gameDisplay.height << std::endl;
     // Create and open a window for the game
     RenderWindow window(vm, "Buzzy's Revenge", Style::Fullscreen);
 
@@ -47,13 +96,11 @@ int main()
     textureCreature[9].loadFromFile("graphics/Georgia_Bulldogs_logo_dog_g.png");
 
 
-    // Create a sprite
-    const int totalLives = 5;
-
+    /* ************************* SPRITES ********************/
+    // Create sprites
     Sprite spriteBackground;
-    Sprite spriteBuzz, spriteLives[totalLives], spriteInsect, spriteCreature[10];
+    Sprite spriteBuzz, spriteLives[totalLives], spriteInsect;
 
-    // Attach the texture to the sprite
     spriteBackground.setTexture(textureBackground);
     spriteBuzz.setTexture(textureBuzz);
     spriteInsect.setTexture(textureInsect);
@@ -75,7 +122,7 @@ int main()
                                         textRect.height / 2.0f);
     }
 
-    //Set Scale for bunny and tiger
+    //Set Scale for tiger
     spriteCreature[0].setScale(1/8.0f, 1/8.0f);
 
     // Power bar
@@ -91,7 +138,7 @@ int main()
     powerBar.setSize(Vector2f(powerBarWidth*power, powerBarHeight));
     powerBar.setFillColor(Color::Red);
 
-    //Create Text Objects
+    /* ************************* Text Object ********************/
     Text titleText;
     Text scoreText;
     Text livesText;
@@ -99,15 +146,14 @@ int main()
     Text powerText;
     Text nameText[2];
 
-    // We need to choose a font
     Font font;
     font.loadFromFile("fonts/KOMIKAP_.ttf");
 
-    // Set the font to our message
     titleText.setFont(font);
     scoreText.setFont(font);
     livesText.setFont(font);
     powerText.setFont(font);
+    plusScore.setFont(font);
 
     for (int i = 0; i < 4 ; i++)
     {
@@ -118,7 +164,6 @@ int main()
     {
         nameText[i].setFont(font);
     }
-
 
     // Assign the actual message
     titleText.setString("Buzzy's Revenge");
@@ -131,10 +176,12 @@ int main()
     powerText.setString("Power ");
     nameText[0].setString(" Created by ");
     nameText[1].setString(" Mehak Arora ");
+    plusScore.setString("0");
 
     titleText.setCharacterSize(100);
     scoreText.setCharacterSize(30);
     livesText.setCharacterSize(30);
+    plusScore.setCharacterSize(30);
     for (int i = 0; i < 4 ; i++)
     {
         messageText[i].setCharacterSize(50);
@@ -150,6 +197,8 @@ int main()
     titleText.setFillColor(Color::Red);
     scoreText.setFillColor(Color::White);
     livesText.setFillColor(Color::White);
+    plusScore.setFillColor(Color::Cyan);
+
     for (int i = 0; i < 4 ; i++)
     {
         messageText[i].setFillColor(Color::White);
@@ -161,9 +210,9 @@ int main()
     }
 
 
-    // Set the spriteBackground to cover the screen
+    /* ************************* Set Positions ********************/
     spriteBackground.setPosition(0, 0);
-
+    plusScore.setPosition(3000,3000);
 
     // Position the text
     textRect = titleText.getLocalBounds();
@@ -204,7 +253,7 @@ int main()
         lastPos += textSpace;
     }
 
-    scoreText.setPosition(1710, 20);
+    scoreText.setPosition(1700, 20);
     livesText.setPosition(30, 33);
     powerText.setPosition(30, 1000);
     powerBar.setPosition(150 ,  1000);
@@ -233,7 +282,7 @@ int main()
 
     }
 
-
+    //Positions of creatures in columns
     Vector2f creaturePos[10];
 
     float startY(1080 / 3.0f );
@@ -253,7 +302,7 @@ int main()
         creaturePos[i].y = startY + (i % 5)*120;
     }
 
-    // Prepare the sound
+    /* *************************** Sounds ***************************** */
     SoundBuffer chopBuffer;
     chopBuffer.loadFromFile("sound/chop.wav");
     Sound chop;
@@ -289,9 +338,9 @@ int main()
     bee.setBuffer(beeBuffer);
 
 
+    /* *************************** Check Variables ***************************** */
 
     bool paused = true;
-    int score = 0;
     bool acceptInput = false;
     bool buzzFlying = false;
     bool buzzFalling = false;
@@ -302,47 +351,32 @@ int main()
     bool disp1(true), disp2(true);
     bool creaturesFalling = false;
     bool creatureCollide = false;
+    bool beeActive = false;
+    bool makeScoreFly = false;
+
+    /* *************************** Other Variables ***************************** */
     int creatureIndex;
     float angle, creatureVelocity(0);
-    bool beeActive = false;
+    int lives = totalLives;
+    int score = 0;
+    int creature;
+
     Vector2f buzzVelocity, beeVelocity;
     Clock clock, beeClock;
-    int lives = totalLives;
 
+    //Setting the initial velocity of Buzz
     float a(-9.8 * 50), tmax(4), tmin(1), vInitialMin, vInitialMax;
     vInitialMin = (1920 - (50))/tmax;
     vInitialMax = (1920 - (50))/tmin;
 
-    int index;
-    int column1[5], column2[5], creatureRest[8] = {1,2,3,4,5,6,7,8};
-    srand(time(0));
-    if(rand()%2 == 0)
-    {
-        column1[4] = TIGER;
-        column2[4] = GEORGIABULLDOG;
-    }
-    else
-    {
-        column1[4] = GEORGIABULLDOG;
-        column2[4] = TIGER;
-    }
+    /* *************************** RANDOM SHUFFLE ***************************** */
 
-    shuffle(creatureRest, creatureRest + 8, std::default_random_engine(time(0) + 2 ));
-    for (int i =0; i < 4; i++)
-    {
-        column1[i] = creatureRest[i];
-        column2[i] = creatureRest[i+4];
-    }
-    shuffle(column1, column1 + 5, std::default_random_engine(time(0) ));
-    shuffle(column2, column2 + 5, std::default_random_engine(time(0) + 1));
+    Shuffle(0, creaturePos);
 
-    for (int i = 0; i < 10 ; i++)
-    {
-        index = (i < 5) ? column1[i]:column2[i-5];
-        spriteCreature[index].setPosition(creaturePos[i].x, creaturePos[i].y);
-    }
-    int counter = 0;
 
+    /* *************************** START GAME LOOP ***************************** */
+
+    int counter = 0; //Makes randomising more random
     while(window.isOpen()){
 
         if (Keyboard::isKeyPressed(Keyboard::Escape))
@@ -353,8 +387,8 @@ int main()
         // Start the game
         if (paused && Keyboard::isKeyPressed(Keyboard::Return))
         {
-
             reset = true;
+            lives = 5;
             score = 0;
             std::stringstream ss;
             ss << "Score = " << score;
@@ -362,6 +396,7 @@ int main()
 
         }
 
+        //Reset score, power, creatures
         if(reset)
         {
             paused = false;
@@ -370,38 +405,10 @@ int main()
             disp2 = true;
             foundUnicorn = false;
 
-            lives = totalLives;
             power = 0;
             powerBar.setSize(Vector2f(powerBarWidth*power, powerBarHeight));
 
-            srand(time(0) + counter);
-            if(rand()%2 == 0)
-            {
-                column1[4] = TIGER;
-                column2[4] = GEORGIABULLDOG;
-            }
-            else
-            {
-                column1[4] = GEORGIABULLDOG;
-                column2[4] = TIGER;
-            }
-
-            shuffle(creatureRest, creatureRest + 8, std::default_random_engine(time(0) + 2 +counter));
-            for (int i =0; i < 4; i++)
-            {
-                column1[i] = creatureRest[i];
-                column2[i] = creatureRest[i+4];
-            }
-            shuffle(column1, column1 + 5, std::default_random_engine(time(0) + counter));
-            shuffle(column2, column2 + 5, std::default_random_engine(time(0) + 1 + counter));
-
-            for (int i = 0; i < 10 ; i++)
-            {
-                index = (i < 5) ? column1[i]:column2[i-5];
-                spriteCreature[index].setPosition(creaturePos[i].x, creaturePos[i].y);
-                spriteCreature[index].setRotation(0);
-            }
-
+            Shuffle(counter, creaturePos);
             spriteBuzz.setPosition(50, (1080 / 2.0f) + 200);
             spriteBuzz.setRotation(-35);
             reset = false;
@@ -409,15 +416,13 @@ int main()
 
         }
 
-
-
         if(!paused)
         {
             Time dt = clock.restart();
             float dtBee;
 
 
-            //BEE
+            /* ****************** MOVE THE BEE RANDOMLY *********************** */
             if (!beeActive)
             {
 
@@ -466,20 +471,21 @@ int main()
 
 
 
-            //BUZZ
+            /* ****************** SET BUZZ ANGLE AND POWER *********************** */
+
             if (acceptInput && !buzzFlying)
             {
                 if (Keyboard::isKeyPressed(Keyboard::Down))
                 {
                     angle = spriteBuzz.getRotation();
-                    spriteBuzz.setRotation(angle + 1e-2);
+                    spriteBuzz.setRotation(angle + 1e-1);
                 }
 
                 if (Keyboard::isKeyPressed(Keyboard::Up))
                 {
 
                     angle = spriteBuzz.getRotation();
-                    spriteBuzz.setRotation(angle - 1e-2);
+                    spriteBuzz.setRotation(angle - 1e-1);
                 }
 
                 if (Keyboard::isKeyPressed(Keyboard::Space))
@@ -491,7 +497,7 @@ int main()
 
                 }
 
-                Event event;
+                Event event; //Listen for space key release
                 while (window.pollEvent(event)) {
                     if (event.type == Event::KeyReleased && event.key.code == Keyboard::Space) {
                         chop.play();
@@ -502,11 +508,9 @@ int main()
                         buzzFlying = true;
                     }
                 }
-
             }
 
-            int creature;
-            //Buzz Flying
+            /* ****************** PROJECTILE MOTION AND COLLISION *********************** */
             if(buzzFlying)
             {
                 spriteBuzz.setPosition(
@@ -574,7 +578,7 @@ int main()
             if(creatureCollide)
             {
                 creature = (creatureIndex < 5) ? column1[creatureIndex]:column2[creatureIndex-5];
-                if(creature == 0 || creature == 9)
+                if(creature == 0 || creature == 9)   //Tiger and Bulldog
                 {
                     score += 25;
                     if(creatureIndex < 5){
@@ -586,8 +590,17 @@ int main()
                     creaturesFalling = true;
                     death.play();
                     acceptInput = false;
+                    std::stringstream ss;
+                    ss << "+" << 25;
+                    plusScore.setString(ss.str());
+                    plusScore.setCharacterSize(30);
+                    plusScore.setPosition(spriteBuzz.getPosition());
+
+                    velocity.x = (1700 - spriteBuzz.getPosition().x)/2;
+                    velocity.y = (30 - spriteBuzz.getPosition().y)/2;
+                    makeScoreFly = true;
                 }
-                else if (creature == 8 && !foundUnicorn){
+                else if (creature == 8 && !foundUnicorn){  //Unicorn
                     foundUnicorn = true;
                     unicorn.play();
                     lives = (lives < 5) ? lives + 1: lives;
@@ -612,7 +625,7 @@ int main()
                     }
 
                 }
-                else{
+                else{   //Woodland Creature
                     lives -= 1;
                     buzzFalling = true;
                     sad.play();
@@ -624,7 +637,7 @@ int main()
                     }
                 }
 
-                if(!buzzFalling){
+                if(!buzzFalling ){
                     spriteBuzz.setPosition(50, (1080 / 2.0f) + 200);
                     spriteBuzz.setRotation(-35);
                 }
@@ -632,34 +645,67 @@ int main()
                 power = 0;
                 powerBar.setSize(Vector2f(powerBarWidth*power, powerBarHeight));
                 creatureCollide = false;
-                std::stringstream ss;
-                ss << "Score = " << score;
-                scoreText.setString(ss.str());
+                if(!makeScoreFly){
+                    std::stringstream ss;
+                    ss << "Score = " << score;
+                    scoreText.setString(ss.str());
+                }
 
             }
 
             if(beeCollide)
             {
-                spriteBuzz.setPosition(50, (1080 / 2.0f) + 200);
-                spriteBuzz.setRotation(-35);
                 bee.play();
+                std::stringstream ss;
+                ss << "+" << 25;
+                plusScore.setString(ss.str());
+                plusScore.setCharacterSize(30);
+                plusScore.setPosition(spriteBuzz.getPosition());
+
+                velocity.x = (1700 - spriteBuzz.getPosition().x)/2;
+                velocity.y = (30 - spriteBuzz.getPosition().y)/2;
+                makeScoreFly = true;
                 power = 0;
                 powerBar.setSize(Vector2f(powerBarWidth*power, powerBarHeight));
+
                 score += 75;
+                spriteBuzz.setPosition(50, (1080 / 2.0f) + 200);
+                spriteBuzz.setRotation(-35);
                 beeCollide = false;
-                std::stringstream ss;
-                ss << "Score = " << score;
-                scoreText.setString(ss.str());
+
+            }
+
+            if(makeScoreFly){
+
+                //std::cout << plusScore.getPosition().x << " " << plusScore.getPosition().y << std::endl;
+                if(plusScore.getPosition().y  > 0 && plusScore.getPosition().x < 1920){
+                    plusScore.setPosition( plusScore.getPosition().x + velocity.x * dt.asSeconds(), plusScore.getPosition().y + velocity.y * dt.asSeconds());
+                    //plusScore.setCharacterSize(plusScore.getCharacterSize() + 5);
+                    acceptInput = false;
+                }
+                else{
+                    plusScore.setPosition(-3000,-3000);
+                    makeScoreFly = false;
+                }
+
+                if(!makeScoreFly){
+                    //spriteBuzz.setPosition(50, (1080 / 2.0f) + 200);
+                    //spriteBuzz.setRotation(-35);
+                    std::stringstream ss;
+                    ss << "Score = " << score;
+                    scoreText.setString(ss.str());
+                    acceptInput = true;
+                }
             }
 
             if(buzzFalling){
-
                 buzzVelocity.y = buzzVelocity.y - a*dt.asSeconds();
                 buzzVelocity.x = 0;
                 spriteBuzz.setPosition(
                         spriteBuzz.getPosition().x,
                         spriteBuzz.getPosition().y + buzzVelocity.y*dt.asSeconds());
-                float rotAngle = 3e-1;
+                float rotAngle = 5e-1;
+                acceptInput = false;
 
                 spriteBuzz.rotate(rotAngle);
 
@@ -674,7 +720,7 @@ int main()
 
             if(creaturesFalling) {
 
-                float rotAngle = 3e-1;
+                float rotAngle = 5e-1;
                 creatureVelocity += a*dt.asSeconds();
                 bool check1(true), check2(true);
 
@@ -694,7 +740,7 @@ int main()
                     if (!disp2)
                     {
                         check2 = false;
-                        if(!foundUnicorn || (foundUnicorn && column1[j]!=8))
+                        if(!foundUnicorn || (foundUnicorn && column2[j]!=8))
                         {
                             spriteCreature[column2[j]].setPosition(
                                     spriteCreature[column2[j ]].getPosition().x,
@@ -716,6 +762,8 @@ int main()
                         check2 = true;
                     }
                 }
+
+                acceptInput = false;
 
                 if (check1 && check2) {
                     creaturesFalling = false;
@@ -744,6 +792,7 @@ int main()
 
         window.draw(spriteBuzz);
         window.draw(spriteInsect);
+        window.draw(plusScore);
 
         for (int i=0; i < lives; i++)
         {
